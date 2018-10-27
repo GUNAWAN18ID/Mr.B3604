b7d6fad715e062d2eb9c3b71333ba3a64d98253b
GUNAWAN18ID <44337431+GUNAWAN18ID@users.noreply.github.com>
2018-10-27 20:37:39 +0700

    Add files via upload

---
+++ Mr.B3604.sh
@@ -0,0 +1,230 @@
+#MAU NGAPAIN MONYET !!
+#MAU RECODE !?
+#HAHAHA KONTOL RECODE AJG !!
+#NGAPAIN MASIH DI SINI MONYET !!
+#PERGI LAGI SANA KONTOL !!
+#BANGSAT LU ANJING !!
+#LU YANG MONYET !!
+#PERGI JAUH JAUH KONTOL BABI !!
+#SALAM HEKEL
+#SALAM MAQLO
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+figlet "TOOLS 407 AUTHENTIC EXPLOIT V1"|lolcat
+
+echo "          [===============TOOLS 3XPLOIT==============]"|lolcat
+echo "          [   AUTHOR  : MR.B3604                     ]"
+echo "          [==========================================]"|lolcat
+echo "          [   TEAM    : 407 AUTH3NTIC 3XPLOIT        ]"
+echo "          [==========================================]"|lolcat
+echo "          [   CONTACT :  +6283896536547              ]"
+echo "          [==========================================]"|lolcat
+
+echo "================================="|lolcat
+echo "[01] iesDEFACE "
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo "[02] SQLMAP "
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo "[03] CRACK WEB VULN (KO-DORK) "
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo "[04] NMAP "
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo "[05] BYPASS ADMIN (ADMIN FINDER) "
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo "[06] AOC DEFACE "
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo "[07] MEMBUAT SCRIPT DEFACE "
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo "[08] REMOTE SHELL "
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo "[09] WEB KILLER "
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo "[10] NGROK "
+echo "================================="|lolcat
+echo "================================="|lolcat
+echo "[11] KELUAR 1!1! "
+echo "================================="|lolcat
+echo "\033[33;1m"
+read -p "SILAHKAN MASUKAN NOMER:" bro
+
+if [ $bro = 01 ] || [ $bro = 01 ]
+then
+clear
+figlet "407 Authentic Exploit" | lolcat
+sleep 1 
+apt install git
+apt install bash
+pip2 install bash
+git clone https://github.com/ALX-04/iesDEFACE
+mv iesDEFACE $HOME
+cd $HOME/iesDEFACE
+bash iesDeface.sh
+fi
+
+if [ $bro = 02 ] || [ $bro = 02 ]
+then
+clear
+toilet -f standard -F gay "407 Authentic Exploit"
+apt update
+apt install git
+git clone https://github.com/sqlmapproject/sqlmap
+mv sqlmap $HOME
+cd $HOME/sqlmap
+read -p "masukan web target:" target
+python2 sqlmap.py -D target
+fi
+
+if [ $bro = 03 ] || [ $bro = 03 ]
+then
+figlet -f smslant "407 Authentic Exploit"|lolcat
+apt upgrade -y
+apt install git -y
+apt install python2 -y
+git clone https://github.com/CiKu370/ko-dork
+cd ko-dork
+sleep 1
+python2 dork.py
+fi
+
+if [ $bro = 04 ] || [ $bro = 04 ]
+then
+figlet -f smslant "407 Authentic Exploit"|lolcat
+apt upgrade -y
+apt install git -y
+git clone https://github.com/Xi4u7/BlackNmap
+cd BlackNmap
+sleep 1
+chmod 777 blacknmap
+./blacknmap
+fi
+
+if [ $bro = 05 ] || [ $bro = 05 ]
+then
+clear
+toilet -f big -F gay "407 Authentic Exploit"
+sleep 1
+apt update 
+apt upgrade
+apt install 
+apt install git
+git clone https://github.com/the-c0d3r/admin-finder.git
+mv admin-finder $HOME
+cd $HOME/admin-finder
+python2 admin-finder.py
+fi
+
+if [ $bro = 06 ] || [ $bro = 06 ]
+then
+clear
+toilet -f big -F gay "407 Authentic Exploit"
+sleep 1
+apt update 
+apt upgrade
+apt install 
+apt install git
+git clone https://github.com/Amriez/AOCDEFACE.git
+cd AOCDEFACE
+sh install.sh
+sleep 1
+sh AOC.sh
+fi
+
+if [ $bro = 07 ] || [ $bro = 07 ]
+then
+clear
+toilet -f big -F gay "407 Authentic Exploit"
+sleep 1
+apt update 
+apt upgrade
+apt install 
+apt install git
+git clone https://github.com/Ubaii/script-deface-creator
+cd script-daface-creator
+chmod +x create.py
+python2 create.py
+fi
+
+if [ $bro = 08 ] || [ $bro = 08 ]
+then
+clear
+toilet -f big -F gay "407 Authentic Exploit"
+sleep 1
+apt update 
+apt upgrade
+apt install 
+apt install git
+apt install perl
+git clone https://github.com/aryanrtm/Remote-Shell
+cd Remote-Shell
+perl remoteshell.pl
+fi
+
+if [ $bro = 09 ] || [ $bro = 09 ]
+then
+clear
+toilet -f big -F gay "407 Authentic Exploit"
+sleep 1
+apt update 
+apt upgrade
+apt install 
+apt install git
+git clone https://github.com/ultrasecurity/webkiller.git
+cd webkiller
+pip install -r requirements.txt
+python webkiller.py
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+toilet -f big -F gay " Mr.B4J1N64N "
+sleep 1
+apt update && apt upgrade
+apt install wget
+mkdir ngrok
+cd ~/ngrok
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+mv ngrok-stable-linux-arm.zip $HOME
+$HOME/unzip ngrok-stable-linux-arm.zip
+cd ~/
+ls
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+echo "\033[31;1mWE ARE 407 AUTHENTIC EXPLOIT"
+sleep 1
+echo "\033[32;1mTerimakasih Sayang Kamu Sudah Berkunjung :*"
+sleep 1
+echo "\033[33;1mJangan Lupa Di Share Ea :*"
+sleep 1
+echo "\033[34;1mMr.B3604"
+sleep 1
+echo "\033[35;1mByee Byee..."
+sleep 1
+exit
+fi
+
